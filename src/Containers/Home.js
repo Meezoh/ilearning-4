b85879f3d7037e33372ee60fcb723753e733db7a
Abdulhameed Hameed <hameed.babba@gmail.com>
2022-04-24 13:55:44 +0200

    first commit

---
+++ src/Containers/Home.js
@@ -0,0 +1,60 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/form';
+import { useState } from 'react';
+import Message from './Message';
+
+// interface Prop {
+//   handleName: any;
+// }
+
+const Home = () => {
+  const [name, setName] = useState('');
+  const [view, setView] = useState(true);
+
+  const handleSignin = e => {
+    e.preventDefault();
+
+    fetch(
+      'https://email-kin.herokuapp.com/api/v1/messages/' + name.toLowerCase()
+    )
+      .then(res => res.json())
+      .then(result => {
+        // handleName(name);
+        setView(false);
+      });
+  };
+
+  const handleClick = () => {
+    if (name.length > 0) setView(false);
+  };
+
+  const handleGoBack = () => {
+    setView(true);
+  };
+
+  return (
+    <div className="enter-name">
+      {view && (
+        <div className={view ? 'container' : 'hidden'}>
+          <h1 className="yo">Your Name Pls!</h1>
+          <Form onSubmit={handleSignin}>
+            <Form.Control
+              type="text"
+              placeholder="Enter your name"
+              onChange={e => setName(e.target.value)}
+              className="input"
+              required
+            />
+            <Button type="submit" className="input" onClick={handleClick}>
+              Sign in
+            </Button>
+          </Form>
+        </div>
+      )}
+
+      {view || <Message name={name} view={view} handleGoBack={handleGoBack} />}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Containers/Message.js
@@ -0,0 +1,85 @@
+import Form from 'react-bootstrap/form';
+import { useState, useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Message = ({ name, view, handleGoBack }) => {
+  const [user, setUser] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const details = { name: newName.toLowerCase(), title, body };
+
+  useEffect(() => {
+    fetch('https://email-kin.herokuapp.com/api/v1/messages/' + name)
+      .then(res => res.json())
+      .then(result => {
+        setUser(
+          result.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+        );
+      });
+  }, [user]);
+
+  const handleMessage = () => {
+    fetch('https://email-kin.herokuapp.com/api/v1/messages/', {
+      method: 'POST',
+      body: JSON.stringify(details),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(res => res.json())
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+  };
+
+  return (
+    <div className={view ? 'Message show' : 'Message'}>
+      <Button className="go-back" onClick={handleGoBack}>
+        Go back
+      </Button>
+      <div className="details">
+        <Form>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Jon Doe"
+              onChange={e => setNewName(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter your title"
+              onChange={e => setTitle(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group
+            className="mb-3"
+            controlId="exampleForm.ControlTextarea1"
+            onChange={e => setBody(e.target.value)}
+          >
+            <Form.Label>Body</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+        <Button className="add-message" onClick={handleMessage}>
+          Send Message
+        </Button>
+        <div>
+          <h2>Messages</h2>
+          <ul>
+            {user.map(user => (
+              <li key={user._id}>
+                <h3>{user.title}</h3>
+                <p>{user.body}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
